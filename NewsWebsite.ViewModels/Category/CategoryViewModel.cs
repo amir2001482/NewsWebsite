cff1e16907e053f00955b221a97425c3de555f03
Amir Lakzaei <lninomls9@gmail.com>
2023-02-19 21:47:53 +0330

    categoryManager 1.1

---
+++ NewsWebsite.ViewModels/Category/CategoryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NewsWebsite.ViewModels.Category
+{
+    public class CategoryViewModel
+    {
+        [JsonProperty("Id")]
+        public string CategoryId { get; set; }
+
+        [Display(Name ="عنوان دسته بندی"), JsonProperty("دسته")]
+        [Required(ErrorMessage ="وارد نمودن {0} الزامی است.")]
+        public string CategoryName { get; set; }
+
+        [JsonProperty("ردیف")]
+        public int Row { get; set; }
+
+        [Display(Name ="دسته پدر"),JsonProperty("دسته پدر")]
+        public string ParentCategoryName { get; set; }
+
+
+        [Display(Name = "آدرس"),JsonProperty("آدرس")]
+        [Required(ErrorMessage = "وارد نمودن {0} الزامی است.")]
+        public string Url { get; set; }
+    }
+}
--- NewsWebsite.ViewModels/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NewsWebsite.ViewModels
-{
-    public class Class1
-    {
-    }
-}
