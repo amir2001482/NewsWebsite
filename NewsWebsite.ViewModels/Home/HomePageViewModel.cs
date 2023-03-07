0964e749daa34123e2b19437e2264b67e2068524
Amir.H lakzaei <amirhosseinlakzaei2000@gmail.com>
2023-03-07 11:22:18 +0330

    SlideShow2

---
+++ NewsWebsite.ViewModels/Home/HomePageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using NewsWebsite.ViewModels.News;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsWebsite.ViewModels.Home
+{
+    public class HomePageViewModel
+    {
+        public HomePageViewModel(List<NewsViewModel> news)
+        {
+            _news = news;
+        }
+        public List<NewsViewModel> _news { get; set; }
+    }
+}
--- NewsWebsite/Controller/HomeController.cs
+++ NewsWebsite/Controller/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NewsWebsite.Data.Contracts;
+using NewsWebsite.ViewModels.Home;
 
 namespace NewsWebsite.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IUnitOfWork _uw;
+
+        public HomeController(IUnitOfWork uw)
         {
-            return View();
+            _uw = uw;
+        }
+       
+        public async Task<IActionResult> Index()
+        {
+            var news = await _uw.NewsRepository.GetPaginateNews(0, 10, null, null, null, null, true, "", true);
+            var homePageViewModel = new HomePageViewModel(news);
+            return View(homePageViewModel);
         }
     }
 }
